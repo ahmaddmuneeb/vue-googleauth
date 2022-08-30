172440320444ba50c0e0bf52409c2d261af2f06b
Muneeb Ahmad <ahmaddmuneeb786@gmail.com>
2022-08-31 03:43:07 +0500

    vue-google-auth

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <GoogleAuth msg=""/>
+</template>
+
+<script>
+import GoogleAuth from './components/GoogleAuth.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GoogleAuth
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+import gAuthPlugin from "vue3-google-oauth2";
+let gauthClientId = "676785461988-icoil0dtlld2fcp5kb22llst7t94mans.apps.googleusercontent.com";
+app.use(gAuthPlugin, {
+  clientId: gauthClientId,
+  scope: "profile email",
+  prompt: "consent",
+  fetch_basic_profile: true,
+});
+
+app.mount("#app");
